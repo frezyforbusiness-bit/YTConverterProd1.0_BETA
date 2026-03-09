d4b48df4d58dbf37606a8f81bf8e1dd0910822f9
fpiumatti <fpiumatti@siapcn.it>
2026-03-09 17:25:03 +0100

    chore: update App.tsx

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import { ErrorBoundary } from './components/common/ErrorBoundary';
